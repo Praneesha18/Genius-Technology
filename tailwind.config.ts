cb22797cd0c74e386ef116e7d9f20b2ca1955593
MounikaDarru <darru.mounika.123@gmail.com>
2025-08-13 19:42:34 +0530

    tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,31 @@ const config: Config = {
   		colors: {
   			primary: "#F5C80D",
   			secondary: "#004AAD",
+  			border: "hsl(var(--border))",
+  			input: "hsl(var(--input))",
+  			ring: "hsl(var(--ring))",
+  			background: "hsl(var(--background))",
+  			foreground: "hsl(var(--foreground))",
+  			card: {
+  				DEFAULT: "hsl(var(--card))",
+  				foreground: "hsl(var(--card-foreground))",
+  			},
+  			popover: {
+  				DEFAULT: "hsl(var(--popover))",
+  				foreground: "hsl(var(--popover-foreground))",
+  			},
+  			muted: {
+  				DEFAULT: "hsl(var(--muted))",
+  				foreground: "hsl(var(--muted-foreground))",
+  			},
+  			accent: {
+  				DEFAULT: "hsl(var(--accent))",
+  				foreground: "hsl(var(--accent-foreground))",
+  			},
+  			destructive: {
+  				DEFAULT: "hsl(var(--destructive))",
+  				foreground: "hsl(var(--destructive-foreground))",
+  			},
   		},
 		fontFamily: {
 			poppins: ["Inter", "sans-serif"],
